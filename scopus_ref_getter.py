6bec4e8c4bda3b16b83bba4bca67036f4f68f8ef
JJEliPark <pkmon111d@gmail.com>
2024-04-30 11:34:08 +0000

    0430

--- scopus_ref_getter.py
+++ scopus_ref_getter.py
@@ -1,8 +1,7 @@
 import pandas as pd
-import numpy as np
 from pybliometrics.scopus import ScopusSearch
 
-pd.set_option('display.max_columns', None)
+# Load data
 data = pd.read_csv("./data/ai_research_papers.csv")
 origin_eids = data['eid']
 origin_eids_set = set(origin_eids)
@@ -10,16 +9,17 @@ origin_eids_set = set(origin_eids)
 columns = ['eid', 'eid_citedby_ai', 'citedby_count_ai', 'eid_citedby_etc', 'citedby_count_etc']
 df = pd.DataFrame(columns=columns)
 
-i = 0
+failed_eids = []  # List to store failed eids
 
-for row in data.iterrows() :
-    i+=1
-    e = row[1][0]
-    try :
-        if row[1]['citedby_count'] == 0 :
-            continue
-        q = "REF(" + e + ")"
-        s = ScopusSearch(q, verbose=True)
+# Process each row
+for i, row in enumerate(data.itertuples(), 1):
+    e = row.eid
+    if row.citedby_count == 0:
+        continue
+
+    q = f"REF({e})"
+    try:
+        s = ScopusSearch(q, verbose=False)
         ref_by = set(s.get_eids())
         in_origin = list(origin_eids_set.intersection(ref_by))
         in_origin_n = len(in_origin)
@@ -30,9 +30,15 @@ for row in data.iterrows() :
         not_origin = ';'.join(not_origin)
         new_row = [e, in_origin, in_origin_n, not_origin, not_origin_n]
         df = df.append(pd.Series(new_row, index=df.columns), ignore_index=True)
-        print("# :", i, "Eid : ", e, "Done.")
-    except :
-        print(e, "Failed")
+        print(f"# : {i}, Eid : {e}, Done.")
+    except Exception as ex:
+        print(f"{e} Failed: {str(ex)}")
+        failed_eids.append(e)
         break
 
+# Save DataFrame to CSV
 df.to_csv("./data/ai_papers_citations.csv")
+
+# Save failed eids to a CSV file
+if failed_eids:
+    pd.DataFrame(failed_eids, columns=['eid']).to_csv('./data/failed_eids.csv')
